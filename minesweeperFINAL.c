c88ab4fe66dea3cf04b3207a522fd678ed282cc2
Jayden-Winselman <150065254+Jayden-Winselman@users.noreply.github.com>
2023-11-27 11:59:01 -0600

    11/27/3023 WIP update

--- minesweeperFINAL.c
+++ minesweeperFINAL.c
@@ -1,53 +1,110 @@
 #include <stdio.h>
-int dif;
+#include <stdlib.h>
+#include <time.h>
 
-void gameboerd(dif){
-    int boardXsize, boardYsize, gameXsize, gameYsize;
-
+int main(){
+int i = 0, x = 0, y = 0;
+int solutionboard[30][16];
+char gameboard[30][16], Move;
+for (y = 0; y < 16; y++) {
+                for (x = 0; x < 30; x++) {
+                        gameboard[x][y] = '?';
+                }
+        }
+int dif, score, playerx = 0, playery = 0;  
+int boardXsize, boardYsize, minecount, minesplaced = 0;
+        printf("let's play minesweeper\nDifficultys\n");
+        printf("1 - beginner     - 10 mines -  9 * 9  grid\n");
+        printf("2 - Intermediate - 40 mines - 16 * 16 grid\n");
+        printf("3 - Advanced     - 99 mines - 30 * 16 grid\n Enter dificulty (#) : ");
+    scanf("%i", &dif);
+    
     switch (dif){
         case 1:
             boardXsize = 9;
             boardYsize = 9;
-            gameXsize = 9;
-            gameYsize = 9;
-             break;
+            minecount = 10;
+            break;
         case 2:
             boardXsize = 16;
             boardYsize = 16;
-            gameXsize = 16;
-            gameYsize = 16;
-             break;
+            minecount = 40;
+            break;
         case 3:
             boardXsize = 30;
             boardYsize = 16;
-            gameXsize = 30;
-            gameYsize = 16;
-             break;
-        }
-    int Sboard[boardXsize][boardYsize] = 0 
-    
-    printf("enter board cordnet (X): \n");
-    printf("enter board cordnet (y): \n");
+            minecount = 99;
+            break;
+        default:
+            printf("Error - Invalid Input");
+            return 0;
+            };
+   /* printf("enter first cordanents to dig\n");
+        
+        printf("enter x-cordnate value: ");
+            scanf("%i", &playerx);
 
+        printf("enter y-cordnate value: ");
+            scanf("%i", &playery);
+*/
+        do {
+            for (y = 0; y < boardYsize; y++) {
+                for (x = 0; x < boardXsize; x++) {
+                    solutionboard[x][y] = '0';
+                    }
+                }
 
-    return 0;
-}
+            while (minesplaced < minecount) {
+                srand(time(0));
+            x = rand() % boardXsize;
+            y = rand() % boardYsize;
+            if (solutionboard[x][y] == '0') {
+                solutionboard[x][y] = '9';
+                minesplaced++;
+                }
+            }
+            /*if((solutionboard[playerx-1][playery-1]) = 0) */{i++;}
+        }while (i = 0);
+    for (y = 0; y < boardYsize; y++) {
+            for (x = 0; x < boardXsize; x++) {
+                if((solutionboard[x][y]) = 9) {}
+                    else{
+                        if(solutionboard[x-1][y-1]){solutionboard[x][y] += 1;} 
+                        if(solutionboard[x][y-1]){solutionboard[x][y] += 1;} 
+                        if(solutionboard[x+1][y-1]){solutionboard[x][y] += 1;} 
+                        
+                        if(solutionboard[x-1][y]){solutionboard[x][y] += 1;} 
+                        if(solutionboard[x+1][y]){solutionboard[x][y] += 1;} 
+                        
+                        if(solutionboard[x-1][y+1]) {solutionboard[x][y] += 1;} 
+                        if(solutionboard[x][y+1])   {solutionboard[x][y] += 1;} 
+                        if(solutionboard[x+1][y+1]) {solutionboard[x][y] += 1;} 
+                        } 
+                    }
+                }
+//print print solotion board for testing
+for(y = 0; y < boardYsize; y++){
+            for(x = 0; x < boardXsize; x++){
+                    printf("%c ", solutionboard[x][y]);
+                }
+            printf("\n");
+            }
+  /*  while(score != (boardXsize * boardYsize - minecount) || score < 999){
+        for(int y = 1; y < boardYsize; y++){
+            for(int x = 0; x < boardXsize; x++){
+                    printf("%c ", gameboard[x][y]);
+                }
+            printf("\n");
+            }
+    printf("Enter your move.    d. dig  f. flag/unflag : ");
+        scanf("%c", Move);
+    printf("enter x-cordnate value: ");
 
+        
+    }
 
-
-int main(){
-        printf("let's play minesweeper\nDifficultys\n");
-        printf("1 - beginner     - 10 mines -  9 * 9  grid\n");
-        printf("2 - Intermediate - 40 mines - 16 * 16 grid\n");
-        printf("3 - Advanced     - 99 mines - 16 * 30 grid\n Enter dificulty (#) : ");
-    scanf("%i", &dif);
-        if (dif>0 && dif<4)
-        {
-            gameboerd(dif);
-        }
-        else
-        {
-        printf("Error - Invalid Input");
-        }
+if(score = (boardXsize * boardYsize - minecount)){printf("congrats you win\n");}
+else{printf("sorry you lost try again\n");}
+ */   
     return 0;
 }
